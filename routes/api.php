d3b66ea1c43b765a92411f7efc534442b3ddfa1f
Oscar Molinar <oscarmolinar96@gmail.com>
2024-09-07 16:44:02 -0500

    Add endpoint for todoist webhooks

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/todoist', function (Request $request) {
+     // Recuperar todo el contenido del request (datos, encabezados, etc.)
+     $requestData = print_r($request->all(), true); // Obtener los datos en formato legible
+     $requestHeaders = print_r($request->headers->all(), true); // Obtener los encabezados
+
+     // Puedes agregar más información como los métodos y la URL del request
+     $additionalInfo = "Method: " . $request->method() . "\n";
+     $additionalInfo .= "URL: " . $request->fullUrl() . "\n";
+
+     // Combinar la información en un string
+     $content = $additionalInfo . "\nHeaders:\n" . $requestHeaders . "\nData:\n" . $requestData;
+
+     // Escribir en un archivo .txt en el storage de Laravel (storage/app/requests.txt)
+     Storage::put('requests.txt', $content);
 });
